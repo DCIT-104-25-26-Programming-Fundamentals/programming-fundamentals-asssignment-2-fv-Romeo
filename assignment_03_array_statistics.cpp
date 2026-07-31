9cb1adba8370f4e0d9288173fc233f35a0af2a74
unknown <vuvlafafanyo@gmail.com>
2026-07-31 16:48:02 +0000

    added the find max function

--- assignment_03_array_statistics.cpp
+++ assignment_03_array_statistics.cpp
@@ -53,3 +53,13 @@ int computeSum(int arr[], int n) {
 double computeAverage(int arr[], int n) {
     return static_cast<double>(computeSum(arr, n)) / n;
 }
+
+int findMax(int arr[], int n) {
+    int maxVal = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > maxVal) {
+            maxVal = arr[i];
+        }
+    }
+    return maxVal;
+}
